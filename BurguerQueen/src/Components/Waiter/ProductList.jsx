fadd98766f2d55a3a0d6bd2322310b4bff15fda3
ValenParaguatey <valentina.paraguatey@gmail.com>
2023-07-25 19:05:31 -0400

    Se suben cambios de la vista de mesero, con funcionalidad de agregar productos y sacar total

--- BurguerQueen/src/Components/Waiter/ProductList.jsx
+++ BurguerQueen/src/Components/Waiter/ProductList.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { requestProduct } from '../../functions/request';
 
-function ProductList(){
+function ProductList({showBreakfast,addToSelectedProducts, removeFromSelectedProducts}){
     const [productsData, setProductsData] = useState([]);
-
+    
     useEffect(()=>{
         async function fetchData() {
             const productsList = await requestProduct();
@@ -15,18 +15,29 @@ function ProductList(){
     }, []);
 
     function renderProductList(){
-        const filteredProducts = productsData.filter(product => product.type === 'Desayuno');
+        let filteredProducts 
+        if(showBreakfast){
+            filteredProducts= productsData.filter(product => product.type === 'Desayuno');
+           
+        } else{
+            filteredProducts= productsData.filter(product => product.type === 'Almuerzo');
+           
+        }
         return filteredProducts.map(product => (
-            <>
+        
             <div key={product.id}>
-            <img src={product.image} alt={product.name} />
+            <img src={product.image} alt={product.name}  className='lg:w-36  lg:h-36'/>
             <p>{product.name}</p>
-            <p>{product.price}</p>
+            <p>$ {product.price}</p>
+            <button onClick={()=> {addToSelectedProducts(product)}}>+</button>
+            <span>{product.quantity}</span>
+            <button onClick={()=> {removeFromSelectedProducts(product)}}>-</button>
             
         </div>
-            </>
+        
         ));
     }
+   
     return (
         <div>
             {renderProductList()}
