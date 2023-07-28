41192ac641431f3eb3f477fc2976501822a27d6b
Mariana-Sanchez21 <marysansam21@gmail.com>
2023-07-28 15:02:21 -0500

    cambios a vista de mesero css

--- BurguerQueen/src/Components/Waiter/WaiterViewBreakfast.jsx
+++ BurguerQueen/src/Components/Waiter/WaiterViewBreakfast.jsx
@@ -61,31 +61,31 @@ const handleOpenModal= () => {
 }
 
     return (
-   <section className=' bg-black'>
- <nav className=' bg-primary lg:h-28 flex justify-between'>
- <div className=' lg:h-24 lg:w-28 fixed'>
+   <section className=' bg-black text-white '>
+ <nav className=' bg-primary lg:h-28 flex justify-between text-black sm:w-full sm:h-20 font-bold'>
+ <div className=' lg:h-24 lg:w-28 sm:h-16 sm:w-20 fixed'>
    <img src={LogoBQ} alt="logo" />
  </div>
- <ul className=' flex justify-between lg:h-16 mt-8 lg:w-h   ml-h  bg-white'> 
-   <li className='lg:mb-20 border-red-600'>Ordenes Listas</li>
-   <li className='lg:mb-20 border-red-600'>Historial de Ordenes</li>
+ <ul className=' flex lg:justify-between lg:h-16 mt-8 lg:w-h  lg:ml-96  sm:h-9 sm:w-80 sm:ml-24 sm:justify-around'> 
+   <li className='lg:mb-20 sm:mb-24 border-red-600 lg:text-3xl'>Ordenes Listas</li>
+   <li className='lg:mb-20 sm:mb-24 border-red-600 lg:text-3xl'>Historial de Ordenes</li>
  </ul>
  </nav>
- <div className='Buttons border-black bg-red-400'>
-   <button onClick={handleOpenModal}>Nueva Orden</button>
+ <div className='Buttons border-black flex lg:justify-around lg:mt-6'>
+   <button onClick={handleOpenModal} className='lg:mr-96 font-bold border-2 border-secondary rounded-sm shadow-lg lg:p-2 lg:text-3xl'>Nueva Orden</button>
    {openModal && (
     <Modal>
       <ClientForm setOpenModal={setOpenModal}/>
     </Modal>
    )}
-    <button onClick={HandleShowBreakfast}>Desayuno</button>
-    <button onClick={HandleShowLunch}>Almuerzo</button>
+    <button onClick={HandleShowBreakfast} className='border-2 border-tertiary rounded-sm shadow-lg lg:-mr-20 lg:p-2 font-bold lg:text-3xl'>Desayuno</button>
+    <button onClick={HandleShowLunch} className=' lg:mr-20 font-bold border-2 border-secondary rounded-sm shadow-lg lg:p-2 lg:text-3xl'>Almuerzo</button>
  </div>
-<article>
+<article className=' lg:w-3/5 '>
 <ProductList showBreakfast={showBreakfast} addToSelectedProducts={addToSelectedProducts}  decreaseFromSelectedProducts={decreaseFromSelectedProducts} removeFromSelectedProducts={removeFromSelectedProducts}/>
 </article>
-<article>
-  <div>
+<article className='flex'>
+  <div className='lg:w-1/4'>
     <p>Resumen de la Orden</p>
     <table>
       <thead>
@@ -101,7 +101,7 @@ const handleOpenModal= () => {
         return (
         <tr key={index}>
           <td>
-            <img src={product.image} alt={product.name} className='lg:w-36 lg:h-36'/>
+            <img src={product.image} alt={product.name} className=' border-secondary rounded-sm shadow-lg lg:p-2 lg:w-36 lg:h-36 border-2'/>
           </td>
           <td>{product.name}</td>
           <td>
