7c7e28c70a6dfa297cd4a210f0589b3a09965d34
ValenParaguatey <valentina.paraguatey@gmail.com>
2023-08-03 18:30:45 -0400

    Cambios en la vista de mesero con la funcionalidad para enviar orden a cocina

--- BurguerQueen/src/Components/Waiter/WaiterViewBreakfast.jsx
+++ BurguerQueen/src/Components/Waiter/WaiterViewBreakfast.jsx
@@ -1,10 +1,10 @@
 import LogoBQ from '../../assets/LogoBQ.png'
 import papelera from '../../assets/papelera.png'
 import { ProductList } from './ProductList'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Modal } from '../Modal/Modal';
 import { ClientForm } from '../ClientForm/ClientForm';
-
+import { requestPostOrder } from '../../functions/request';
 
 
 function WaiterViewBreakfast(){
@@ -13,15 +13,13 @@ function WaiterViewBreakfast(){
   const[openModal, setOpenModal] = useState(false);
   const [clientInfo, setClientInfo] = useState('');
   const [clientTable, setClientTable] = useState('');
-  
-  
+  const [orderData, setOrderData] = useState([]);
+ 
   const HandleShowBreakfast = () => {
     setShowBreakfast(true)
-    console.log('Clic para desayuno')
   };
   const HandleShowLunch = () => {
-    setShowBreakfast(false)
-    console.log('Clic para Almuerzo')
+    setShowBreakfast(false)   
   }
   const calculateTotal = () => {
     return selectedProducts.reduce((total, product) => total + product.price * product.quantity, 0);
@@ -73,6 +71,45 @@ function WaiterViewBreakfast(){
 
 };
 
+const onSubmitOrder = async (e) => {
+  e.preventDefault();
+  try {
+    if (clientInfo && clientTable && selectedProducts.length > 0) {
+      const token = localStorage.getItem('token');
+      const orderData = {
+        clientName: clientInfo,
+        tableNumber: clientTable,
+        products: selectedProducts,
+      };
+      const orderDataString = JSON.stringify(orderData);
+      localStorage.setItem('orderData', orderDataString);
+
+      
+      const sendOrderToKitchen = () => {
+        const orderDataString = localStorage.getItem('orderData');
+        if (orderDataString) {
+          const orderData = JSON.parse(orderDataString);
+        
+          console.log(orderData);
+        } else {
+          console.log('No order data found in local storage.');
+        }
+      };
+
+      sendOrderToKitchen();
+
+      setOrderData([]);
+      localStorage.removeItem('orderData');
+    } else {
+      console.log("Por favor, ingrese el nombre del cliente, el número de mesa y seleccione al menos un producto.");
+    }
+  } catch (error) {
+    console.log('error recibiendo orden');
+  }
+};
+
+
+
 const handleOpenModal= () => {
   setOpenModal(true);
 }
@@ -98,26 +135,26 @@ const handleOpenModal= () => {
      <button onClick={HandleShowBreakfast} className='border-4 border-tertiary rounded-sm shadow-lg lg:-mr-20 lg:p-2 font-bold lg:text-xl font-retro1 hover:scale-125'>Desayuno</button>
     <button onClick={HandleShowLunch} className=' lg:mr-20 font-bold border-4 border-secondary rounded-sm shadow-lg lg:p-2 lg:text-xl font-retro1 hover:scale-125 md:mr-36'>Almuerzo</button>
  </div>
- <section className='flex justify-between'> 
- <article className=' lg:w-3/5 md:mt-6 md:ml-7'>
+ <section className='flex justify-between md:mt-6'> 
+ <article className=' lg:w-3/5 md:mt-14 md:ml-7'>
 <ProductList showBreakfast={showBreakfast} addToSelectedProducts={addToSelectedProducts}  decreaseFromSelectedProducts={decreaseFromSelectedProducts} removeFromSelectedProducts={removeFromSelectedProducts}/>
 </article>
-<article className=' bg-primary h-auto lg:w-h lg:mt-14 lg:mr-16 md:mt-6 md:w-h'>
+<article className=' bg-primary h-auto lg:w-h lg:mt-14 lg:mr-16 md:mt-6 md:w-h '>
   <div className='lg:w-1/4 '>
-    <p className='font-retro1 lg:text-2xl whitespace-nowrap lg:ml-5 lg:mb-12 lg:mt-10 md:mt-8 md:ml-5'>Resumen de la Orden</p>
+    <p className='font-retro1 lg:text-2xl whitespace-nowrap lg:ml-5 lg:mb-12 lg:mt-10 md:mt-8 md:ml-9 md:text-2xl'>Resumen de la Orden</p>
     <div id='datosCliente flex'>
-      <div>
-      <p className=' whitespace-nowrap lg:text-xl lg:font-bold font-retro2 lg:ml-2 md:text-xl' >Nombre del cliente: {clientInfo}</p>
-      <p className=' whitespace-nowrap lg:text-xl lg:font-bold font-retro2 lg:ml-2 md:text-xl'>Número de mesa: {clientTable}</p> 
+      <div className='md:ml-12 md:w-56'>
+      <p className=' whitespace-nowrap lg:text-xl lg:font-bold font-retro2 lg:ml-2 md:text-2xl md:mt-5' >Nombre del cliente: {clientInfo}</p>
+      <p className=' whitespace-nowrap lg:text-xl lg:font-bold font-retro2 lg:ml-2 md:text-2xl'>Número de mesa: {clientTable}</p> 
       </div>
-      <div className='lg:ml-96 lg:-mt-12 lg:mb-12'>
-      <button className=" font-extrabold text-2xl bg-btn2 w-9" onClick={clearClientInfo} type="button">X</button>
+      <div className='lg:ml-96 lg:-mt-12 lg:mb-12 md:-mt-12 md:mb-11'>
+      <button className=" font-extrabold text-2xl bg-btn2 w-9 lg:ml-8 md:ml-mtboton" onClick={clearClientInfo} type="button">X</button>
       </div>
     </div>
     <table>
       <thead> 
         <tr>
-          <th className='lg:pl-9 lg:mt-12 font-retro2 lg:text-xl md:text-xl '>Imagen</th>
+          <th className='lg:pl-9 lg:mt-12 font-retro2 lg:text-xl md:text-xl md:pl-8 '>Imagen</th>
           <th className='lg:pl-9 lg:mt-12 font-retro2 lg:text-xl md:text-xl md:pl-6'>Producto</th>
           <th className='lg:pl-9 lg:mt-12 font-retro2 lg:text-xl md:text-xl md:pl-6'>Cantidad</th>
           <th className='lg:pl-9 lg:mt-12 font-retro2 lg:text-xl md:text-xl md:pl-6'> Precio</th>
@@ -128,13 +165,13 @@ const handleOpenModal= () => {
         return (
         <tr key={index}>
           <td className='lg:pl-9'>
-            <img src={product.image} alt={product.name} className=' lg:p-2 lg:w-96 lg:h-20 lg:mt-5 md:w-24 md:h-24'/>
+            <img src={product.image} alt={product.name} className=' lg:p-2 lg:w-96 lg:h-20 lg:mt-5 md:w-24 md:h-24 md:pl-6'/>
           </td>
-          <td className='lg:pl-12 font-retro2 lg:text-lg md:text-lg md:pl-10'>{product.name}</td>
-          <td className='lg:pl-16 font-retro2 lg:text-lg md:text-lg md:pl-12'>
+          <td className='lg:pl-12 font-retro2 lg:text-lg md:text-xl md:pl-10'>{product.name}</td>
+          <td className='lg:pl-16 font-retro2 lg:text-lg md:text-xl md:pl-12'>
             {product.quantity}
           </td>
-          <td className='lg:pl-14 font-retro2 lg:text-lg md:pl-9'>${product.price * product.quantity}</td>  
+          <td className='lg:pl-14 font-retro2 lg:text-lg md:pl-9 md:text-xl'>${product.price * product.quantity}</td>  
           <td>
             <div className='bg-blue w-16 h-16'>
           <button onClick={()=> {removeFromSelectedProducts(product)}}>
@@ -149,10 +186,10 @@ const handleOpenModal= () => {
       </tbody>
     </table>
   </div>
-  <p className='font-retro2 lg:text-2xl lg:mt-12 lg:ml-9 md:mt-12 md:text-2xl'>Total: ${calculateTotal()}</p>
-  <div className='flex justify-between lg:w-96 lg:ml-20 lg:mt-10 lg:mb-5 md:ml-2 md:mt-7'>
-      <button className=" border-4 border-black rounded-md lg:w-44 lg:h-20 text-2xl font-extrabold bg-btn1 hover:scale-125 md:text-xl md:w-32 md:h-16" type="submit">Mandar a cocina</button>
-      <button className=" border-4 border-black rounded-md lg:w-44 lg:h-20 text-2xl  font-extrabold lg:ml-12  bg-btn2 hover:scale-125 md:text-xl md:w-32 md:h-16 md:mr-32" type="button" onClick={clearOrder} >Cancelar Orden</button>
+  <p className='font-retro2 lg:text-2xl lg:mt-12 lg:ml-9 md:mt-12 md:text-2xl md:pl-8'>Total: ${calculateTotal()}</p>
+  <div className='flex md:justify-around lg:w-mtboton lg:ml-8 lg:mt-10 lg:mb-5 md:ml-2 md:mt-7'>
+      <button className=" border-4 border-black rounded-md lg:w-96 lg:h-20 text-2xl font-extrabold bg-btn1 hover:scale-125 md:text-xl md:w-32 md:h-16 md:ml-12" type="submit" onClick={onSubmitOrder}>Mandar a cocina</button>
+      <button className=" border-4 border-black rounded-md lg:w-96 lg:h-20 text-2xl  font-extrabold lg:ml-8  bg-btn2 hover:scale-125 md:text-xl md:w-32 md:h-16 md:mr-24" type="button" onClick={clearOrder} >Cancelar Orden</button>
 </div>
 </article> 
 
