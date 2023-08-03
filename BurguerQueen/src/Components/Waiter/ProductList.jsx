309980c387347a558b6fa293b3e67b75695f645e
ValenParaguatey <valentina.paraguatey@gmail.com>
2023-08-03 10:00:13 -0400

    se suben cambios con responsive para vista de ipad añadidos

--- BurguerQueen/src/Components/Waiter/ProductList.jsx
+++ BurguerQueen/src/Components/Waiter/ProductList.jsx
@@ -27,18 +27,18 @@ function ProductList({showBreakfast,addToSelectedProducts, decreaseFromSelectedP
         }
         return filteredProducts.map(product => (
         
-            <div className='lg:p-5' key={product.id}>
-            <img src={product.image} alt={product.name}  className=' lg:w-44   lg:h-44 border-4 border-secondary rounded-sm shadow-lg lg:p-2 '/>
+            <div className='lg:p-5 ' key={product.id}>
+            <img src={product.image} alt={product.name}  className=' lg:w-44 lg:h-44 border-4 border-secondary rounded-sm shadow-lg lg:p-2 md:w-36 md:h-32 '/>
            <div className='flex lg:mt-5'> 
-            <p className='lg:text-3xl font-retro2 lg:mr-6'>{product.name}</p>
-            <p className='lg:text-3xl font-retro2'>$ {product.price}</p>
+            <p className='lg:text-3xl font-retro2 lg:mr-6 md:text-xl md:ml-3'>{product.name}</p>
+            <p className='lg:text-3xl font-retro2 md:text-xl md:ml-2'>${product.price}</p>
             </div>
             <button onClick={()=> {addToSelectedProducts(product)}}>
-                <img className='lg:w-6 lg:h-5 lg:mr-12 lg:ml-8 lg:mt-5 hover:scale-125' src={signoMas} alt="mas" />
+                <img className='lg:w-6 lg:h-5 lg:mr-12 lg:ml-8 lg:mt-5 hover:scale-125 md:w-6 md:h-4 md:mt-3 md:ml-8 md:mb-3'  src={signoMas} alt="mas" />
             </button>
             <span>{product.quantity}</span>
             <button onClick={()=> {decreaseFromSelectedProducts(product)}}>
-                <img className='lg:w-5 lg:h-2 lg:mb-1 hover:scale-125' src={signoMenos} alt="menos" />
+                <img className='lg:w-5 lg:h-2 lg:mb-1 hover:scale-125 md:w-4 md:h-2 md:ml-8 md:mb-3' src={signoMenos} alt="menos" />
 
             </button>
             
